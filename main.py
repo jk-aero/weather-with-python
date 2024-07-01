88e45d56e5f6c56dccd11227b879eb2eec5746f2
jk-aero <92267875+jk-aero@users.noreply.github.com>
2024-07-02 00:18:00 +0530

    Update main.py
    can fetch data from weather api
    (open weather map)

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-# to download image using the image url
+import os
 import requests
-r=requests.get('https://cloud-f4lij7sq1-hack-club-bot.vercel.app/0flowerpcb.png')
 
-with open("imageName.png",'wb') as f:
-    f.write(r.content)
+APIKEY=os.environ.get('weatherAPI') #hiding api key in the system environment variable
+print(APIKEY) 
+location=input("Enter the city Name:")
+#api call 
+complete_api_link ="https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+APIKEY
+
+
+api_link=requests.get(complete_api_link)
+api_data=api_link.json() # getting the json data from the link
+print(api_data) # printing the whole json object
+
