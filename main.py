0419b4d8be57d33457d3dc7e950635dbdf9dd9dd
jk-aero <92267875+jk-aero@users.noreply.github.com>
2024-07-02 00:38:10 +0530

    Update main.py
    print data which we fetched

--- main.py
+++ main.py
@@ -10,5 +10,22 @@ complete_api_link ="https://api.openweathermap.org/data/2.5/weather?q="+location
 
 api_link=requests.get(complete_api_link)
 api_data=api_link.json() # getting the json data from the link
-print(api_data) # printing the whole json object
+#print(api_data)
 
+if api_data['cod']=='404':
+    print("some error occured,check your city name".format(location))
+
+else:
+    weather_desc=(api_data['weather'][0]['description'])
+    temp_city=((api_data['main']['temp'])-273.15)
+    humidity_city=(api_data['main']['humidity'])
+    windspeed_city=(api_data['wind']['speed'])
+
+
+print(weather_desc)
+print(temp_city)
+print('; degree celcius\n')
+print(humidity_city)
+print('; pascal\n')
+print(windspeed_city)
+print('; kilometer/hour\n')
